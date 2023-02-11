5bc99e136c4209ad6ddcb3206ca48529043cbfd5
Gabriel Mayorga <gabrielmayorgate@gmail.com>
2023-02-10 19:42:40 -0600

    Nueva versión terminada, falta adición de palabras

--- js/ahorcado.js
+++ js/ahorcado.js
@@ -1,5 +1,7 @@
 let gword = null
 let state = 0
+let cant = 0
+let correct = 0
 
 function random(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -37,7 +39,7 @@ function word(){
 }
 
 function letter(letter){
-    if (state !== 6){
+    if (state !== 7){
         const botton = document.querySelector(`#${letter.toLowerCase()}`)
         botton.disabled = true
         botton.classList.remove("enable")
@@ -48,49 +50,83 @@ function letter(letter){
             }
             sletter.textContent = letter
             botton.classList.add("correct")
+            correct+=1
+            if(correct===cant){
+                const win_menu = document.getElementById('win')
+                win_menu.classList.remove('hidde')
+                const buttons = document.querySelectorAll('.enable')
+                for(var j = 0; j < buttons.length; j++){
+                    buttons[j].disabled = true
+                    buttons[j].classList.add('block')
+                }
+            }
 
         }else{
             let img = document.querySelector("#img")
             switch (state) {
                 case 0:
-                    img.src = "img/ahorcado-1.png"
+                    img.src = "img/Ahorcado-1.png"
                     state = 1
                     botton.classList.add("incorrect")
                     break;
                 case 1:
-                    img.src = "img/ahorcado-2.png"
+                    img.src = "img/Ahorcado-2.png"
                     state = 2
                     botton.classList.add("incorrect")
                     break;
                 case 2:
-                    img.src = "img/ahorcado-3.png"
+                    img.src = "img/Ahorcado-3.png"
                     state = 3
                     botton.classList.add("incorrect")
                     break;
                 case 3:
-                    img.src = "img/ahorcado-4.png"
+                    img.src = "img/Ahorcado-4.png"
                     state = 4
                     botton.classList.add("incorrect")
                     break;
                 case 4:
-                    img.src = "img/ahorcado-5.png"
+                    img.src = "img/Ahorcado-5.png"
                     state = 5
                     botton.classList.add("incorrect")
                     break;
                 case 5:
-                    img.src = "img/ahorcado-6.png"
+                    img.src = "img/Ahorcado-6.png"
                     state = 6
+                    botton.classList.add("incorrect")
+                    break;
+                case 6:
+                    img.src = "img/Ahorcado-7.png"
+                    state = 7
                     botton.classList.add("incorrect")
                     break;
             }
         }
     }
 
-    if (state === 6){
-        if (confirm("Perdiste, ¿quieres volver a jugar?")){
-            reload()
+    if (state === 7){
+        const defeat_menu = document.getElementById('defeat')
+        defeat_menu.classList.remove('hidde')
+        const buttons = document.querySelectorAll('.enable')
+        for(var i = 0; i < buttons.length; i++){
+            buttons[i].disabled = true
+            buttons[i].classList.add('block')
+        }
+
+        const lletters = document.querySelectorAll('.letter')
+        for(var i = 0; i < gword.length; i++){
+            lletters[i].textContent = gword[i]
         }
     }
+}
+
+function defeat_hidde(){
+    const menu = document.getElementById('defeat')
+    menu.classList.add('hidde')
+}
+
+function win_hidde(){
+    const menu = document.getElementById('win')
+    menu.classList.add('hidde')
 }
 
 function reload(){
